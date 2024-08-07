490f24d9fae3006669cc283d3e3664504a57c1be
Aliihsanhashas <aliihsanhashas1@hotmail.com>
2024-08-07 11:13:28 +0300

    Add account, transaction, and loan services

---
+++ CoreBanking/loan-service/src/test/java/com/bilgeadam/banking/loan_service/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.banking.loan_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
