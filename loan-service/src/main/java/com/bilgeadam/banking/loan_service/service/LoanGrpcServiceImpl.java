6302c1347d82074b00cff0e150bb3cd8b94c0e4a
Aliihsanhashas <aliihsanhashas1@hotmail.com>
2024-08-26 13:17:54 +0300

    noTask : Implements LoanGrpcServiceImpl.java and loan.proto

---
+++ loan-service/src/main/java/com/bilgeadam/banking/loan_service/service/LoanGrpcServiceImpl.java
@@ -0,0 +1,9 @@
+package com.bilgeadam.banking.loan_service.service;
+
+import com.bilgeadam.banking.LoanServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class LoanGrpcServiceImpl extends LoanServiceGrpc.LoanServiceImplBase {
+
+}
