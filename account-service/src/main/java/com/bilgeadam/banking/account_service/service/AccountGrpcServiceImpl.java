351596b27a6ebf1f3ffd241ce180b5879bc9d15d
msutebay <msutebay@gmail.com>
2024-08-27 16:58:42 +0300

    no-task: implemented/fixed gRPC server

--- account-service/src/main/java/com/bilgeadam/banking/account_service/service/AccountGrpcServiceImpl.java
+++ account-service/src/main/java/com/bilgeadam/banking/account_service/service/AccountGrpcServiceImpl.java
@@ -1,13 +1,7 @@
 package com.bilgeadam.banking.account_service.service;
 
+import com.bilgeadam.banking.Account;
 import com.bilgeadam.banking.AccountServiceGrpc;
-import com.bilgeadam.banking.GetBalanceRequest;
-import com.bilgeadam.banking.GetBalanceResponse;
-import com.bilgeadam.banking.account_service.domain.Account;
-import com.bilgeadam.banking.account_service.entity.AccountEntity;
-import com.bilgeadam.banking.account_service.exception.AccountNotFoundException;
-import com.bilgeadam.banking.account_service.mapper.AccountMapper;
-import com.bilgeadam.banking.account_service.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
@@ -16,4 +10,13 @@ import java.math.BigDecimal;
 @GrpcService
 public class AccountGrpcServiceImpl extends AccountServiceGrpc.AccountServiceImplBase {
 
+    @Override
+    public void getBalance(Account.GetBalanceRequest request, StreamObserver<Account.GetBalanceResponse> responseObserver) {
+        System.out.println("request to grpc");
+        Account.GetBalanceResponse response = Account.GetBalanceResponse.newBuilder()
+                .setBalance("1001")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
